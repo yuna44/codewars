7a6308566a33b81c6ed7fdc44500d7ada35b06fe
Yuna <yupak987@gmail.com>
2023-11-17 21:33:15 -0500

    update

--- 8kyu/cockroach.js
+++ 8kyu/cockroach.js
@@ -18,3 +18,4 @@ function cockroachSpeed(s) {
 function cockroachSpeed(s) {
   return Math.floor(s*100000/3600);
 }
+
---
+++ Study/to-square-or-not.js
@@ -0,0 +1,29 @@
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+
+
+function squareOrSquareRoot(array) {
+  return array.map(x => {
+    const r = Math.sqrt(x);
+    return (r % 1 == 0) ? r : (x*x);
+  });  
+}
+
+// This one is a better solution //
+function squareOrSquareRoot(array) {
+  return array.map(x => {
+    const n = Math.sqrt(x)
+    return Number.isInteger(n) ? n : x * x
+  })
+}
